8c8f7c83b08379f8454ffef3cb7b0d935d0f9080
jwehring <jwehring@tamu.edu>
2018-11-26 17:07:26 -0600

    sorry again

--- GetMyReview/app/urls.py
+++ GetMyReview/app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
- from . import views
- urlpatterns = [
+from . import views
+urlpatterns = [
   path('', views.index, name='index'),
   path('user/<str:id>/', views.user_profile, name='profile'),
   path('database/', views.database, name='database'),
